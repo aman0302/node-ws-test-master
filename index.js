f41b0f049929a12441ab9e89b0c2aea1a3feb14b
agupta14 <agupta14@ebay.com>
2015-12-12 17:22:39 +0530

    working mqtt

--- index.js
+++ index.js
@@ -14,15 +14,48 @@ console.log("http server listening on %d", port)
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
+
+var net = require('net');
+var mqtt = require('mqtt');
+console.log("calling mqtt");
+var client = mqtt.connect('mqtt://test.mosquitto.org');
+
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
+  /*var id = setInterval(function() {
     ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+  }, 1000)*/
+  
+  
 
   console.log("websocket connection open")
+  
+  
+  
+  ws.on('message',function incoming(message) {  
+    console.log('received: %s', message);
+    client.subscribe(message);
+      client.publish(message, 'Connected');
+      
+      console.log("subscribe"+ client.subscribe(message));
+  })
+  
+  client.on('message', function (topic, payload, packet) {
+
+    console.log(topic + '=' + payload);
+    ws.send(
+        String(payload)
+    );
+});
+    
 
   ws.on("close", function() {
     console.log("websocket connection close")
-    clearInterval(id)
+    //clearInterval(id)
   })
 })
+
+
+
+
+
+
